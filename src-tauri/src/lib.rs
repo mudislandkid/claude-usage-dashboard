8442fc7384e0760373e6b79a73beb02c95ddbc71
Mudislandkid <greg.herriott@outlook.com>
2026-05-24 22:37:06 +0100

    feat(updater): wire updater + process plugins, web-side check on app start

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -16,6 +16,7 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .menu(|app| menu::build(app))
         .on_menu_event(|app, event| {
             match event.id().as_ref() {
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from './App';
+import { checkForUpdates } from './lib/updater';
 import './index.css';
 
 const qc = new QueryClient({
@@ -20,3 +21,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     </QueryClientProvider>
   </React.StrictMode>,
 );
+
+// Fire-and-forget update check 3s after mount (don't block initial paint).
+setTimeout(() => {
+  void checkForUpdates();
+}, 3000);
